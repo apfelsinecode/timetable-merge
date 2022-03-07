1c732076b08b020dca76eafe9789b1216fa0f9a5
apfelsinecode <apfelsinecode@users.noreply.github.com>
2022-03-07 01:30:22 +0100

    time and date

--- python/query_bayern_fahrplan.py
+++ python/query_bayern_fahrplan.py
@@ -10,25 +10,41 @@ departures_url = "https://www.bayern-fahrplan.de/de/abfahrt-ankunft/xhr_departur
 stations = {
     "Würzburg": "80001152",
     "Busbahnhof": "80029081",
+    "Juliuspromenade": "3700208",
+    "Dom": "3700106",
+    "Rathaus": "3700315",
+    "Neubaustraße": "3700271",
+    "Wirsbergstraße": "3700104",
     "Sanderring": "3700348",
+    "Sanderglacisstraße": "3700346",
 }
 
 
-def get_departure_trs(station_id: str = stations["Sanderring"]):
+def get_departure_trs(station_id: str = stations["Sanderring"], date: str = None, time: str = None):
+    """
+
+    :param station_id:
+    :param date: format DD.MM.YYYY
+    :param time: format HH:MM
+    :return:
+    """
     params = {
         "is_fs": "1",
         "is_xhr": "True",
-        # "incl_mot5": "1",
         "nameInfo_dm": station_id
     }
+    if date:
+        params["itdDateDayMonthYear"] = date
+    if time:
+        params["itdTime"] = time
     r = requests.get(departures_url, params=params)
     soup = BeautifulSoup(r.text, "html.parser")
     result = soup.find_all(attrs={"class": "results-tbody"})[0]
     return [c for c in result.children if (type(c) is element.Tag and c.name == "tr")]
 
 
-def departures(station_id: str = stations["Sanderring"]):
-    ds = [Departure(c) for c in get_departure_trs(station_id)]
+def departures(station_id: str = stations["Sanderring"], date: str = None, time: str = None):
+    ds = [Departure(c) for c in get_departure_trs(station_id, date, time)]
     return ds
 
 
